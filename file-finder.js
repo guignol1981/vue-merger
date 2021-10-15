add5f128ae5b9d48e66beb5af9d6b62dfbf90a1f
yumijinno <eyjid@ulaval.ca>
2021-10-15 14:43:30 -0400

    fix deep flag

--- file-finder.js
+++ file-finder.js
@@ -6,7 +6,7 @@ function findFiles(callback, deep = false) {
         fs.readdirSync(currentPath).forEach((e) => {
             const filePath = path.join(currentPath, e);
 
-            if (fs.lstatSync(filePath).isDirectory() && !deep) {
+            if (fs.lstatSync(filePath).isDirectory() && deep) {
                 exploreDirectoryTree(filePath);
             } else if (e.includes('.html')) {
                 callback(filePath.replace('.html', ''));
@@ -16,5 +16,4 @@ function findFiles(callback, deep = false) {
 
     exploreDirectoryTree();
 }
-
 export default findFiles;
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+#!/usr/bin/env node
+
 import merge from './vue-merger.js';
 import findFiles from './file-finder.js';
 
-const deep = !!(process.argv[2] ?? 0);
-
-console.log(deep);
+const deep = !!(process.argv[2] || 0);
 
 findFiles(merge, deep);
